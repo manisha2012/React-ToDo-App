a6c4bd19e294f13022dfd239e4fd899ea2ce6a0c
manisha2012 <manishaa2012@gmail.com>
2018-01-16 00:35:26 +0530

    Added static List component

---
+++ app/components/ToDo.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var ToDo = React.createClass({
+  render: function () {
+    var {text, id} = this.props;
+    return (
+      <div>{id}. {text}</div>
+    );
+  }
+});
+
+module.exports = ToDo;
--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,9 +1,27 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos : [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Feed the cat'
+        }, {
+          id: 3,
+          text: 'Water the plants'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
     return (
-      <div>To Do App rendered</div>
+      <ToDoList todos={todos}/>
     );
   }
 });
