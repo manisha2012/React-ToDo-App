ec47bdeb7a75a83fa1be844857a5c479a459ba08
manisha2012 <manishaa2012@gmail.com>
2018-01-21 23:35:48 +0530

    Add actions

---
+++ app/actions/actions.jsx
@@ -0,0 +1,26 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
+};
+
+export var addTodo = (todo) => {
+  return {
+    type: 'ADD_TODO',
+    todo
+  };
+};
+
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
