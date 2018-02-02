2e39e8953ea661bc848fbd02b006b16da35f00b1
manisha2012 <manishaa2012@gmail.com>
2018-02-03 00:48:03 +0530

    Store new todos in firebase using thunk

---
+++ app/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+try {
+  // Copied from firebase.google.com - Add Firebase To your web app
+  var config = {
+      apiKey: "AIzaSyBWZIhfNko7VwK1b2MIZpRs0KlWXeDeliI",
+      authDomain: "todo-app-f574a.firebaseapp.com",
+      databaseURL: "https://todo-app-f574a.firebaseio.com",
+      projectId: "todo-app-f574a",
+      storageBucket: "todo-app-f574a.appspot.com",
+      messagingSenderId: "89941182708"
+    };
+
+  firebase.initializeApp(config);
+} catch (e) {
+
+}
+
+export var firebaseRef = firebase.database().ref();
+export default firebase;
